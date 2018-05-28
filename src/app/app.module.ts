e2adddd60aa9c0ede4715e806f7cf1ca25a76c67
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-05-27 20:17:53 -0500

    CSC 2018/05/27: Uso del estilo para el login y re-definición de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
 
 //Rutas
 import { APP_ROUTES } from './app.routes';
+import { PagesComponent } from './pages/pages.component';
 
 
 
@@ -26,7 +27,8 @@ import { APP_ROUTES } from './app.routes';
     Graficas1Component,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
