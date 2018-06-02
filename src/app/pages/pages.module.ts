927be7141f4d1d1b77effad69bed191601c3c8f3
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-06-02 16:04:56 -0500

    CSC 2018/06/02: Uso de rutas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,7 @@ import { Graficas1Component } from "./graficas1/graficas1.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { PagesComponent } from "./pages.component";
 import { SharedModule } from "../shared/shared.module";
+import { PAGES_ROUTES } from "./pages.routes";
 
 
 
@@ -20,7 +21,8 @@ import { SharedModule } from "../shared/shared.module";
     Graficas1Component
    ],
    imports: [
-    SharedModule 
+    SharedModule,
+    PAGES_ROUTES
    ]
 })
 export class PagesModule {}
