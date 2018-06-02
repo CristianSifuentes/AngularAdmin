ec737179c933e707480d548d62ac2d47a2597f9a
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-06-02 18:27:00 -0500

    CSC 2018/06/02: Creación de componente personalizado de incrementador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -25,11 +27,12 @@ import { PagesModule } from './pages/pages.module';
     AppComponent,
     LoginComponent,
     RegisterComponent
-  ],
+    ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,8 +6,11 @@ import { Graficas1Component } from "./graficas1/graficas1.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { PagesComponent } from "./pages.component";
 import { SharedModule } from "../shared/shared.module";
+
 import { PAGES_ROUTES } from "./pages.routes";
 
+//temporal
+import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
 
 
 @NgModule({
@@ -15,7 +18,8 @@ import { PAGES_ROUTES } from "./pages.routes";
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent
    ],
    exports: [
     DashboardComponent,
