2befc4b4cef4603cf83e7b7e9dccc4b0e72365bc
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-07-05 14:34:28 -0500

    CSC 2018/07/05: Entendiendo el operador filter

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subscriber } from 'rxjs';
-import { retry, map } from 'rxjs/operators';
+import { retry, map, filter } from 'rxjs/operators';
 
 
 @Component({
@@ -51,7 +51,15 @@ export class RxjsComponent implements OnInit {
               }*/
       }, 1000);
      }).pipe(  
-        map( resp => resp.valor )
+        map( resp => resp.valor ), 
+        filter((valor, index) => {
+             if((valor%2) ===1){
+                return true;
+             }else{
+              return false;
+
+             }
+        })
      );
 
   }
