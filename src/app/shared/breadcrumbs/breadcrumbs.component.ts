e2eb73e6ff847e92561841f76c73632279d7aa9b
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-07-11 19:37:03 -0500

    CSC 2018/07/11: Uso de title

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -10,11 +11,13 @@ import { filter, map } from 'rxjs/operators';
 export class BreadcrumbsComponent implements OnInit {
 
   titulo: string;
-  constructor( private router: Router) { 
+  constructor( private router: Router, 
+    private title: Title) { 
 
     this.getDataRoute().subscribe(data => {
              console.log(event);
              this.titulo = data.titulo;
+             this.title.setTitle(this.titulo);
      });
 
   }
