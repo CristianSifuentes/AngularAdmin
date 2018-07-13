dcfdfa791f5b1885dff4a820aa6be531c7d5a225
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-07-13 15:16:38 -0500

    CSC 2018/07/13: Conexión con MongoDb

--- Backend-Server/app.js
+++ Backend-Server/app.js
@@ -1,11 +1,17 @@
 //Requires
 var express = require('express');
-
+var mongoose = require('mongoose');
 
 //inicializar variables
 var app = express();
 
 
+//Conexión con la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDb', (err, rep) => {
+    if(err) throw err;
+    console.log('Base de datos: \x1b[32m%s\x1b[0m','online');
+
+})
 
 //Rutas
 app.get('/', (req, res, next) => {
