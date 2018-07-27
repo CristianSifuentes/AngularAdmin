9f57ff61a5fac9a152843dfbab1406bc85fa4683
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-07-27 15:15:04 -0500

    CSC 2018/07/27: Método de actualización de usuario.

--- Backend-Server/routes/usuario.js
+++ Backend-Server/routes/usuario.js
@@ -30,6 +30,55 @@ app.get('/', (req, res, next) => {
 
 
 // =============================================
+// Actualizar un nuevo usuario
+// =============================================
+app.put('/:id', (req, res, next) => {
+  
+    var id = req.params.id;
+  var body = req.body;
+
+
+  Usuario.findById(id, (err, usuario) => {
+
+        if(err){
+            return res.status(500).json({ 
+                    ok: false, 
+                    mensje: 'Error al obtener el usuario', 
+                    errors: err 
+           });
+        }
+
+        if(!usuario){
+            return res.status(400).json({ 
+                ok: false, 
+                mensje: 'El usuario  con el id ' + id +' no existe', 
+                errors: { message: 'No existe un usuario con ese ID' }
+            });
+        }
+        
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save(( err, usuarioGuardado) => {
+           
+            if(err){
+                return res.status(400).json({ 
+                        ok: false, 
+                        mensje: 'Error al actualizar el usuario', 
+                        errors: err 
+               });
+            }
+
+            res.status(200).json({ ok: true, usuario: usuarioGuardado });
+
+        });
+  });
+});
+
+
+
+// =============================================
 // Crear un nuevo usuario
 // =============================================
 app.post('/', (req, res, next) => {
