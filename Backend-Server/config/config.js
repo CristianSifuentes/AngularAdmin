e5dfc880b16dcdc350e7aae27690b53ba36635f7
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-08-16 20:50:18 -0500

    CSC 2018/08/16: Backend para el token de google

--- Backend-Server/config/config.js
+++ Backend-Server/config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//google
+module.exports.CLIENT_ID = '495133086740-uat2tm9sa6k4dj569es27mehhsub8bam.apps.googleusercontent.com';
--- Backend-Server/routes/login.js
+++ Backend-Server/routes/login.js
@@ -1,3 +1,4 @@
+import { async } from '@angular/core/testing';
 var express = require('express');
 var bcrypt = require('bcryptjs');
 var app = express();
@@ -5,6 +6,60 @@ var Usuario = require('../models/usuario');
 var jwt = require('jsonwebtoken');
 var SEED = require('../config/config').SEED;
 
+//Google
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+
+
+//=============================================0
+// Autenticación por google
+//=============================================0
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+  }
+
+app.post('/google', async (req, res) =>{
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token).catch(e => {
+        return res.status(403).json({ 
+            ok: false, 
+            mensje: 'Token no válido'
+         });
+    });
+
+    return res.status(200).json({ 
+        ok: true, 
+        mensje: 'ok!!',
+        googleUser : googleUser
+    });
+});
+
+
+//=============================================0
+// Autenticación normal
+//=============================================0
 app.post('/', (req, res) => {
     var body = req.body;
     
