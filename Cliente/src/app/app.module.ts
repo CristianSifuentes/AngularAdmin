716a85ed5736f7f0539ac8a13a975c3f8c4ee9e2
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-08-17 18:55:28 -0500

    CSC 2018/08/17: Formulario de registro

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 //Rutas
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 //Modulos
 import { PagesModule } from './pages/pages.module';
 import { ServiceModule } from './services/service.module';
+
+
+//Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -35,7 +30,9 @@ import { ServiceModule } from './services/service.module';
   BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    ServiceModule
   ],
   providers: [ServiceModule],
   bootstrap: [AppComponent]
