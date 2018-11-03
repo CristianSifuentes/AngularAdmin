ff1906f8796af412c4133445c00dda2971e7abb2
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-11-03 13:59:18 -0600

    CSc 2018/11:03:Usuario pipe

--- Cliente/src/app/pipes/imagen.pipe.ts
+++ Cliente/src/app/pipes/imagen.pipe.ts
@@ -1,3 +1,4 @@
+import { URL_SERVICIOS } from './../config/config';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,7 +6,33 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(img: string, tipo: string = 'usuario'): any {
+
+    let url = URL_SERVICIOS + '/img';
+    if(!img){
+      return url + '/usuarios/xxx';
+    }
+    
+    if(img.indexOf('https') >= 0){
+      return img;
+    }
+
+    switch (tipo){
+      case 'usuario':
+          url += '/usuarios/' + img;
+      break;
+      case 'medico':
+          url += '/medicos/' + img;
+      break;
+      case 'hospital':
+          url += '/usuarios/' + img;
+      break;
+      default:
+         console.log('Tipo de imagen no existe');
+         url += '/usuarios/xxx';
+
+    }
+
     return 'Funciona';
   }
 
